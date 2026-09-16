14ab82cd400799638ee84a1b517b8102bc857499
chloeberthelot <berthelotchloe@outlook.fr>
2026-09-16 14:19:52 +0200

    Ajout d'une notification rappel anniversaire aujourd'hui

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,25 @@ import "./App.css";
 import BirthdayForm from "./components/BirthdayForm";
 import BirthdayList from "./components/BirthdayList";
 import "./css/shared.css";
+import { daysUntilBirthday } from "./utils/birthday";
 
 function App() {
   const [people, setPeople] = useState(() => {
     const stored = localStorage.getItem("people");
     return stored ? JSON.parse(stored) : [];
   });
+
+  const [notif, setNotif] = useState(Notification.permission);
+  useEffect(() => {
+    if (notif === "granted") {
+      const todaysBirthdays = people.filter(
+        (person) => daysUntilBirthday(person.dob) === 0,
+      );
+      todaysBirthdays.forEach((person) => {
+        new Notification(`it's ${person.name} birthday today`);
+      });
+    }
+  }, [people, notif]);
 
   useEffect(() => {
     localStorage.setItem("people", JSON.stringify(people));
@@ -26,6 +39,17 @@ function App() {
       <h1>Birthday List</h1>
       <BirthdayForm onAdd={addPerson} />
       <BirthdayList people={people} onDelete={deletePerson} />
+      <div>
+        <button
+          onClick={() => {
+            Notification.requestPermission().then((permission) => {
+              setNotif(permission);
+            });
+          }}
+        >
+          Activer les rappels
+        </button>
+      </div>
     </div>
   );
 }
